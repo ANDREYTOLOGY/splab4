61a464a7f60b565634927e2d5f1c3c404feeaf1d
ANDREYTOLOGY <63473361+ANDREYTOLOGY@users.noreply.github.com>
2021-03-29 18:42:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,37 +7,35 @@ import subprocess
 import os
 import hashlib
 
-def hash_string(string): # получаем хэш от строки
+def hash_string(string): 
 	h = hashlib.new('md5') 
 	h.update(string.encode()) 
 	return h.hexdigest() 
 
 
-path = '.'  # директория, в которой будем смотреть файлы. Точка значит, что в директории где находится исходный файл
+path = '.'  
 
-processes = []  # массив, в котором мы будем хранить дескрипторы от запущенных процессов
+processes = []  
+files = os.listdir(path)  
+for file in files:  
+	fullpath = os.path.join(path, file)  
+	if os.path.isdir(fullpath):
+		continue  
 
-files = os.listdir(path)  # смотрим файлы по нужному пути
-for file in files:  # для каждого файла в директории
-	fullpath = os.path.join(path, file)  # получаем его полный путь
-	if os.path.isdir(fullpath): # проверяем - является ли файл директорией?
-		continue  # если да - он нам не нужен, пропускаем
+	print(f'{fullpath} found')  
 
-	print(f'{fullpath} found')  # иначе пишем что обрабатываем
-
-	process = subprocess.Popen(  # запускаем процесс
-		["python3", "hasher.py", fullpath],  # fullpath станет 2ым аргументом в вызванной программе
+	process = subprocess.Popen(  
+		["python3", "hasher.py", fullpath],  
 		stdout=subprocess.PIPE
 	)
-	processes.append(process)  # запоминаем созданный дескриптор
+	processes.append(process)  
 
-result = []  # создаем переменную, в которую сложим хэши от файла
+result = []  
 
-for process in processes:  # для каждого процесса
-	process.wait()  # ждем окончания процесса
-	out, err = process.communicate()  # получаем то, что процесс писал в stdout и stderr
-	data = out.decode()  # декодируем то, что он отправил в stdout
-	result.append(data)  # добавляем результат
+for process in processes:  
+	process.wait() 
+	out, err = process.communicate()  
+	data = out.decode()  
+	result.append(data)  
 
-# собираем все хэши в одну большую строку, которую хэшим, после чего итоговый хэш выводим на экран
 print(hash_string(''.join(result)))
