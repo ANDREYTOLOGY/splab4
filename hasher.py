369222af006860ec0ecc8b5833a3f7a33bc89e02
ANDREYTOLOGY <63473361+ANDREYTOLOGY@users.noreply.github.com>
2021-03-29 18:40:39 +0700

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -1,17 +1,17 @@
 import sys
 import hashlib
 
-def hash_file(path): # получение хэша от файла
-	h = hashlib.new('md5')  # создаем хэшер
-	with open(path, 'rb') as f:  # открываем файл на чтение по байтам
+def hash_file(path): 
+	h = hashlib.new('md5')  
+	with open(path, 'rb') as f:  
 		while True: 
-			block = f.read(1024) # читаем кусками по 1024 байта
-			if not block:  # если файл кончился - выходим
+			block = f.read(1024) 
+			if not block:  
 				break 
-			h.update(block) # хэшируем
+			h.update(block) 
 	
-	return h.hexdigest()  # возвращаем хэш в виде hex-значений
+	return h.hexdigest()  
 
 if __name__ == '__main__':
-	filepath = sys.argv[1] # взятие второго аргумента командной строки (пути к файлу)
-	print(hash_file(filepath)) # вывод в консоль результата хэширования содержимого файла
+	filepath = sys.argv[1] 
+	print(hash_file(filepath)) 
